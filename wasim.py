ff717a3ea67e60cc319408770e73d900ed491395
wasim-s-creator <sayyadwasim99@gmail.com>
2025-09-15 16:18:42 +0530

    syntax error check

--- wasim.py
+++ wasim.py
@@ -1,8 +1,31 @@
-﻿# TODO: improve logic 1x
-print('Hello AI SDLC!')
-def greet():
-    print("Hello AI SDLC!")
-    # Example change
-print("Hello AI SDLC test!")
+﻿
 
- 
+# --- Class addition ---
+class Calculator:
+    def __init__(self):
+        self.value = 0
+
+    def add(self, x, y):
+        print("Adding numbers") 
+        return x + y
+
+    def subtract(self, x, y):
+        return x - y
+
+# --- Function addition ---
+def multiply(a, b):
+    return a * b  # Simple function
+
+# --- Function with TODO ---
+def divide(a, b):
+   
+    return a / b
+
+
+def syntax_error_example()
+    print("This will fail")  
+
+
+x = 10
+y = 20
+z = x + y
